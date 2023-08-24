b0118ca68707f60582913fa6074c435557985c87
CardinalCandle <87134232+CardinalCandle@users.noreply.github.com>
2023-08-24 18:41:53 +0100

    Update alx_check_all.js
    Refactored the code

--- alx_check_all.js
+++ alx_check_all.js
@@ -1,52 +1,29 @@
 // ==UserScript==
 // @name     ALX check all
-// @version  0.1
+// @version  0.2
 // @grant    none
 // @include http://intranet.alxswe.com/projects/*
 // @include https://intranet.alxswe.com/projects/*
 // @require http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
+
 $(document).ready(function() {
-const modalOpen_xpath = '//*[contains(@class, "check-your-task")]';
-const modalClose_xpath = '//center/button[contains(@class, "close-modal")]';
-const check_xpath = '//center/button[contains(@class, "test_send")]';
+  const check_xpath = '//center/button[contains(@class, "test_send")]';
 
-var xpath = function (xpathToExecute) {
-    var result = [];
-    var nodesSnapshot = document.evaluate(xpathToExecute, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-    for (var i = 0; i < nodesSnapshot.snapshotLength; i++) {
-        result.push(nodesSnapshot.snapshotItem(i));
+  function clickCheckboxes() {
+    // Capture current scroll position
+    const initialScrollPosition = window.scrollY || window.pageYOffset;
+
+    // Click the checkboxes
+    const checkboxes = document.evaluate(check_xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+    for (let i = 0; i < checkboxes.snapshotLength; i++) {
+      checkboxes.snapshotItem(i).click();
     }
-    return result;
-}
 
+    // Restore scroll position
+    window.scrollTo(0, initialScrollPosition);
+  }
 
-function buttonClick (path) {
-  xpath(path).forEach(button => button.click());
-  console.log('button clicked');
-  return;
-}
-  
-  function lockA () {
-  var xPos = window.scrollX;
-  var yPos = window.scrollY;
-  window.onscroll = () => window.scroll(xPos, yPos);
-}
-  function unlock () { window.onscroll = ""; }
-  
-  
-setInterval(function () {
-	// Capture current scroll position
-	const initialScrollPosition = window.scrollY || window.pageYOffset;
-  buttonClick(modalOpen_xpath);
-  buttonClick(check_xpath);
-  buttonClick(modalClose_xpath);
-  // Restore scroll position
-  window.scrollTo(0, initialScrollPosition);
-	
-	}, 25000);
-  return;
-  
-
-  
+  // Click checkboxes when the script is executed
+  clickCheckboxes();
 });
